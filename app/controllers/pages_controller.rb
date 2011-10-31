84010e541ba83048ae7f870536b5de51038c5e07
Krzysztof Walasek <inocenty@poczta.onet.pl>
2011-10-31 23:05:48 +0100

    After generating Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+    print "tu jestem 1"
+     return "Boba loba from home'oba"
+  end
+
+  def contact
+    print "tu jestem 2"
+     return "Kicha micha out of contact'icha"
+  end
+
+end

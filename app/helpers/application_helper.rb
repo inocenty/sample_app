399cc2b0ddffd911aaf4822f394a529826760a8d
Krzysztof Walasek <inocenty@poczta.onet.pl>
2011-11-02 14:01:47 +0100

    Application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      czas = Time.new
+      "#{base_title} | #{@title} " + czas.getlocal.strftime("%Y-%m-%d %H:%M")
+    end
+  end
 end
